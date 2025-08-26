49b6a3ddd417d7d4ae488c5064b5f5725f1a4c56
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-08-26 23:15:16 +0530

    On Change Event

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,17 @@
 import "./AddTask.css";
+import React, { useState } from "react";
 
 export const AddTask = () => {
+
+    const[taskValue, setTaskValue] = useState("");
+
   return (
     <section className="addtask">
         <form>
-            <label htmlFor="task">Task Name:</label>
-            <input type="text" name="task" id="task" placeholder="Task Name" autoComplete="off"/>
+            <input onChange={(e) => setTaskValue(e.target.value)} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off"/>
             <button type="submit">Add Task</button>
         </form>
+        <p>{taskValue.length}</p>
     </section>
   )
 }
